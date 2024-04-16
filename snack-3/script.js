e95beec667e19192ac98120b639657a13ce550d2
LucaElisio <personal.elix@gmail.com>
2024-04-16 12:00:06 +0200

    fix: snack-3

--- snack-3/script.js
+++ snack-3/script.js
@@ -15,57 +15,21 @@ const automobili = [
     { marca: "Opel", modello: "Corsa", alimentazione: "gpl" }
 ];
 
-/**
- * Description: funzione che ritorna un array di oggetti che hanno alimentazione a benzina
- * @param {array} arrayToCheck
- * @returns {array}
- */
-function gasCar (arrayToCheck) {
-    // Utilizzo filter per andare a copiare solo gli elementi che soddisfano una condizione
-    const gasCarArray = arrayToCheck.filter(function(curItem) {
-        // Se alimentazione corrisponde a benzina, l'oggetto viene inserito nel nuovo array
-        return curItem.alimentazione === "benzina";
-    });
+const dieselCar = [];
+const gasCar = [];
+const otherCar = [];
 
-    return gasCarArray;
-
-};
-
-/**
- * Description: funzione che ritorna un array di oggetti che hanno alimentazione a diesel
- * @param {array} arrayToCheck
- * @returns {array}
- */
-function dieselCar (arrayToCheck) {
-    // Utilizzo filter per andare a copiare solo gli elementi che soddisfano una condizione
-    const dieselCarArray = arrayToCheck.filter(function(curItem) {
-        // Se alimentazione corrisponde a benzina, l'oggetto viene inserito nel nuovo array
-        return curItem.alimentazione === "diesel";
-    });
-
-    return dieselCarArray;
-
-};
-
-/**
- * Description: funzione che ritorna un array di oggetti che non hanno alimentazione benzine ne diesel
- * @param {array} arrayToCheck
- * @returns {array}
- */
-function otherCar (arrayToCheck) {
-    // Utilizzo filter per andare a copiare solo gli elementi che soddisfano una condizione
-    const otherCarArray = arrayToCheck.filter(function(curItem) {
-        // Se alimentazione corrisponde a benzina, l'oggetto viene inserito nel nuovo array
-        return curItem.alimentazione != "diesel" && curItem.alimentazione != "benzina";
-    });
-
-    return otherCarArray;
-
-};
+automobili.forEach((curCar) => {
+    const {alimentazione} = curCar;
+    if (alimentazione === "benzina") {
+        gasCar.push(curCar);
+    } else if (alimentazione === "diesel") {
+        dieselCar.push(curCar);
+    } else {
+        otherCar.push(curCar);
+    }
+});
 
 
-
-console.log(gasCar(automobili));
-console.log(dieselCar(automobili));
-console.log(otherCar(automobili));
+console.log(gasCar, dieselCar, otherCar);
 
